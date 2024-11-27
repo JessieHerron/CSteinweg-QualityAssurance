0fd59253d25e1d569ab38a324f83d79c2093e0f8
Jessie Herron <j.herron@nl.steinweg.com>
2024-11-27 12:14:18 +0200

    create project

---
+++ QASandbox/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace QASandbox
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

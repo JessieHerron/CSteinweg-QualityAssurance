b6248d0c1f6b779895e43091cdd33f38d629ce2b
Jessie Herron <j.herron@nl.steinweg.com>
2024-11-27 14:54:39 +0200

    create http test for export planner api

--- QASandbox/UnitTest1.cs
+++ QASandbox/UnitTest1.cs
@@ -1,16 +1,31 @@
 namespace QASandbox
 {
-    public class Tests
+    public class ExportPlannerAPITests
     {
+        private ApplicationLauncher? launcher;
+        private static readonly HttpClient client = new();
+
         [SetUp]
         public void Setup()
         {
+            launcher = new ApplicationLauncher();
+            launcher.Setup();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            launcher.Teardown();
         }
 
         [Test]
-        public void Test1()
+        public async Task GivenExportPlannerAPI_WhenPageIsLaunched_ThenReturnHttp200Result()
         {
-            Assert.Pass();
+            string url = launcher.GetApplicationUrl();
+            launcher.LaunchApplication(url);
+
+            HttpResponseMessage response = await client.GetAsync(url);
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK), $"Failed to launch {url} with HTTP 200 status code.");
         }
     }
 }
